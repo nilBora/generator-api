3bd6838613c875ef973eef9fada6edd06caf626f
Nil Borodulia <nil.b@oxtech.org>
2023-01-31 13:43:26 +0300

    change method

--- src/ValueObject/Method.php
+++ src/ValueObject/Method.php
@@ -4,11 +4,27 @@ namespace Jtrw\ApiCreator\ValueObject;
 
 class Method
 {
+    private string $name;
+    
     private string $limit;
-    private Property $properties;
+    /**
+     * @var Property[]
+     */
+    private array $properties;
     
-    public function __construct(string $name)
+    public function __construct(string $name, int $limit, array $properties)
     {
+        $this->name = $name;
+        $this->limit = $limit;
+        $this->properties = static::setProperties($properties);
+    }
     
+    private static function setProperties(array $properties)
+    {
+        $vos = [];
+        foreach ($properties as $name => $property) {
+            $vos[] = new Property($property['type'], $name);
+        }
+        return $vos;
     }
 }
